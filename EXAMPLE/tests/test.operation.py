91631163b737218ada3a7c483829fc0d58520717
Chinna-1234 <kchinnamasthanbabu@gmail.com>
2025-09-18 15:45:47 +0530

    unit test case updated

---
+++ EXAMPLE/tests/test.operation.py
@@ -0,0 +1,10 @@
+from src.math_operations import add,sub
+
+def test_add():
+    assert add(2,3)==5
+    assert add(5,4)==9
+    
+def test_sub():
+    assert sub(3,2)==1
+    assert sub(7,3)==4
+         
